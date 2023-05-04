14199a9d97c37729cb947e51b0dcfaad48185b3d
Allink <arclicious@vivaldi.net>
2023-05-04 19:59:19 +0100

    feat: convert dark gray to gray, too

--- src/main/java/com/github/allinkdev/tfutil/TFUtil.java
+++ src/main/java/com/github/allinkdev/tfutil/TFUtil.java
@@ -7,6 +7,7 @@ import java.util.Map;
 public final class TFUtil implements ClientModInitializer {
     public static final Map<Integer, Integer> COLOUR_VALUE_REPLACEMENTS = Map.ofEntries(
             Map.entry(0, 11184810), // Black -> Gray
+            Map.entry(5592405, 11184810), // Dark Gray -> Gray
             Map.entry(170, 5592575), // Dark Blue -> Blue
             Map.entry(43520, 5635925), // Dark Green -> Green
             Map.entry(43690, 5636095), // Dark Aqua -> Aqua
