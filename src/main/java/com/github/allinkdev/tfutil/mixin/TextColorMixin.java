d1717ad717998b3829356a55752d40cb7e128f05
Allink <arclicious@vivaldi.net>
2023-05-04 20:02:56 +0100

    feat: cache interpolated RGB colours

--- src/main/java/com/github/allinkdev/tfutil/mixin/TextColorMixin.java
+++ src/main/java/com/github/allinkdev/tfutil/mixin/TextColorMixin.java
@@ -1,6 +1,8 @@
 package com.github.allinkdev.tfutil.mixin;
 
 import com.github.allinkdev.tfutil.Tuple;
+import net.jodah.expiringmap.ExpirationPolicy;
+import net.jodah.expiringmap.ExpiringMap;
 import net.minecraft.text.TextColor;
 import net.minecraft.util.Formatting;
 import org.spongepowered.asm.mixin.Final;
@@ -10,12 +12,17 @@ import org.spongepowered.asm.mixin.Shadow;
 
 import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Removes RGB colors from the game
  */
 @Mixin(TextColor.class)
 final class TextColorMixin {
+    private static final Map<Integer, TextColor> RGB_INTERPOLATION_CACHE = ExpiringMap.builder()
+            .expiration(5, TimeUnit.MINUTES)
+            .expirationPolicy(ExpirationPolicy.ACCESSED)
+            .build();
     private static final TextColor BLACK = TextColor.fromFormatting(Formatting.BLACK);
     private static final int BLACK_RGB = Objects.requireNonNull(BLACK).getRgb();
     @Shadow
@@ -28,6 +35,10 @@ final class TextColorMixin {
      */
     @Overwrite
     public static TextColor fromRgb(final int rgb) {
+        if (RGB_INTERPOLATION_CACHE.containsKey(rgb)) {
+            return RGB_INTERPOLATION_CACHE.get(rgb);
+        }
+
         Tuple<TextColor, Integer, Integer> closest = Tuple.from(BLACK, BLACK_RGB, rgb - BLACK_RGB);
 
         for (final Map.Entry<Formatting, TextColor> entry : FORMATTING_TO_COLOR.entrySet()) {
@@ -44,6 +55,9 @@ final class TextColorMixin {
             closest = Tuple.from(value, allowedRgb, difference);
         }
 
-        return closest.first();
+        final TextColor interpolatedValue = closest.first();
+        RGB_INTERPOLATION_CACHE.put(rgb, interpolatedValue);
+
+        return interpolatedValue;
     }
 }
